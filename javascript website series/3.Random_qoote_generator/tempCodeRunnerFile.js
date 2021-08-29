d3f7097e6bd8d3b562ed1879dbc0c502e2fdf5b7
Amanjeet Kumar <techamanjeet1@gmail.com>
2021-08-29 11:31:04 +0530

    added 9 basic website

---
+++ javascript website series/3.Random_qoote_generator/tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+console.log(quote.quote[0]);
+        // let bt = document.querySelector(".btn");
+        // bt.addEventListener("click",()=>{
+        //     const no = Math.floor(Math.random()*quote.quote.length)
+        //     document.querySelector(".quote").innerHTML = quote.quote[no].quote;
+        //     document.querySelector(".author").innerHTML = quote.quote[no].author;
+        // })
---
+++ javascript website series/3.Random_qoote_generator/test.js
@@ -0,0 +1,19 @@
+// // const array = require('./array');
+// // console.log(array.array);
+
+// const quote = require('./quote')
+//         console.log(quote.quote[0].text);
+//         console.log(quote.quote[1].author);
+//         console.log(quote.quote[2]);
+// import { array } from './level1.js';
+// console.log(quote.array[0]);
+        // console.log(quote.quote[0]);
+        // let bt = document.querySelector(".btn");
+        // bt.addEventListener("click",()=>{
+        //     const no = Math.floor(Math.random()*quote.quote.length)
+        //     document.querySelector(".quote").innerHTML = quote.quote[no].quote;
+        //     document.querySelector(".author").innerHTML = quote.quote[no].author;
+        // })
+
+import { array } from "./array";
+console.log(array);
